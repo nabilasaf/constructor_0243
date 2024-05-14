bd32fd4bf4abaf8ba18ede2bb3db3b6b89917186
nabilasaf <nabilasafitri861@gmail.com>
2024-05-14 15:31:47 +0700

    constructor

---
+++ constructor.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+    public:
+      int nim;
+      string nama;
+
+    public:
+       mahasiswa()
+       {
+        nim = 0;
+        nama = "";
+       };
+       mahasiswa(int iNim)
+       {
+        nim = iNim;
+       };
+
+       mahasiswa(string iNama)
+       {
+        nama = iNama;
+       };
+
+       mahasiswa(int iNim, string iNama)
+       {
+        nim = iNim;
+        nama = iNama;
+       };
+       void cetak(){
+        cout << "NIM = " << nim << endl;
+        cout << "Nama = " << nama << endl;
+        cout << endl;
+       };
+};
